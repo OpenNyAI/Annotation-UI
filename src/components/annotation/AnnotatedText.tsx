59f2153e1a3b6d0a6b2511b2a414ba39164efa9c
Naresh <chakali.naresh@thoughtworks.com>
2024-06-04 13:30:02 +0530

    chore: extract styles into seperate object

--- src/components/annotation/AnnotatedText.tsx
+++ src/components/annotation/AnnotatedText.tsx
@@ -1,6 +1,7 @@
 import { Delete } from "@mui/icons-material";
 import { Box, IconButton, Typography } from "@mui/material";
 import React, { MouseEvent, useState } from "react";
+import { Styles } from "../../types/styles";
 
 type AnnotatedTextProps = {
   text: string;
@@ -9,6 +10,30 @@ type AnnotatedTextProps = {
   onDragStart(): void;
   onDragEnter(): void;
   onDragEnd(): void;
+};
+
+const styles: Styles = {
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    padding: "16px",
+    alignItems: "center",
+    "& :hover": {
+      cursor: "pointer",
+    },
+    justifyContent: "space-between",
+    border: "1px solid #e0e0e0",
+    borderRadius: "8px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+    backgroundColor: "#3B3B3B",
+  },
+  annotatedText: {
+    display: "-webkit-box",
+    overflow: "hidden",
+    WebkitBoxOrient: "vertical",
+    WebkitLineClamp: 3,
+    fontSize: "16px",
+  },
 };
 
 export const AnnotatedText = ({
@@ -39,22 +64,11 @@ export const AnnotatedText = ({
   return (
     <Box
       component={"div"}
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        padding: "16px",
-        alignItems: "center",
-        "& :hover": {
-          cursor: "pointer",
-        },
-        justifyContent: "space-between",
-        border: "1px solid #e0e0e0",
-        borderRadius: "8px",
-        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-        backgroundColor: "#3B3B3B",
+      style={{
         borderLeft: isHovered ? "4px solid #89CFF0" : "1px solid #e0e0e0",
         borderBottom: isHovered ? "4px solid #89CFF0" : "1px solid #e0e0e0",
       }}
+      sx={styles.container}
       onClick={onClick}
       draggable
       onMouseLeave={() => setIsHovered(false)}
@@ -67,17 +81,7 @@ export const AnnotatedText = ({
         onDragEnd();
       }}
     >
-      <Typography
-        sx={{
-          display: "-webkit-box",
-          overflow: "hidden",
-          WebkitBoxOrient: "vertical",
-          WebkitLineClamp: 3,
-          fontSize: "16px",
-        }}
-      >
-        {text}
-      </Typography>
+      <Typography sx={styles.annotatedText}>{text}</Typography>
       <IconButton
         data-testid="annotation-delete-button"
         size="small"
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,6 +7,7 @@ import { AnnotationSummarySidePanel } from "../../components/annotation/Annotati
 import TextAnnotator, {
   TextAnnotation,
 } from "../../components/annotation/TextAnnotator";
+import { Styles } from "../../types/styles";
 
 const dummyText = `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
 
@@ -36,6 +37,46 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
 `;
 
 const dummyResults = new Array(2).fill(result);
+
+const styles: Styles = {
+  container: {
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+    flexDirection: "row",
+    overflow: "hidden",
+  },
+  mainEditor: {
+    display: "flex",
+    flexDirection: "column",
+    overflow: "scroll",
+    flex: 1,
+    boxSizing: "border-box",
+    p: 4,
+    gap: 4,
+  },
+  mainEditorBox: {
+    border: (theme) => `0.5px solid ${theme.palette.primary.light}`,
+    borderRadius: "8px",
+    height: "40%",
+  },
+  resultsContainer: {
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    flex: "1",
+    gap: 2,
+  },
+  resultBox: {
+    display: "flex",
+    overflow: "scroll",
+    flexDirection: "column",
+    border: (theme) => `0.5px solid ${theme.palette.primary.light}`,
+    borderRadius: "8px",
+    mb: "24px",
+    maxHeight: "340px",
+  },
+};
 
 type QuestionAnswer = {
   question: string;
@@ -82,33 +123,9 @@ export const Home = () => {
   };
 
   return (
-    <Box
-      sx={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        flexDirection: "row",
-        overflow: "hidden",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          overflow: "scroll",
-          flex: 1,
-          boxSizing: "border-box",
-          p: 4,
-          gap: 4,
-        }}
-      >
-        <Box
-          sx={{
-            border: (theme) => `0.5px solid ${theme.palette.primary.light}`,
-            borderRadius: "8px",
-            height: "40%",
-          }}
-        >
+    <Box sx={styles.container}>
+      <Box sx={styles.mainEditor}>
+        <Box sx={styles.mainEditorBox}>
           <TextAnnotator
             id="main-editor"
             text={dummyText}
@@ -118,15 +135,7 @@ export const Home = () => {
             onTextAnnotation={handleTextAnnotation}
           />
         </Box>
-        <Box
-          sx={{
-            position: "relative",
-            display: "flex",
-            flexDirection: "column",
-            flex: "1",
-            gap: 2,
-          }}
-        >
+        <Box sx={styles.resultsContainer}>
           <LabelledInput
             label={<Typography variant="subtitle1">Question</Typography>}
             size="small"
@@ -147,19 +156,7 @@ export const Home = () => {
               (item) => item.id == id
             );
             return (
-              <Box
-                key={index}
-                sx={{
-                  display: "flex",
-                  overflow: "scroll",
-                  flexDirection: "column",
-                  border: (theme) =>
-                    `0.5px solid ${theme.palette.primary.light}`,
-                  borderRadius: "8px",
-                  mb: "24px",
-                  maxHeight: "340px",
-                }}
-              >
+              <Box key={index} sx={styles.resultBox}>
                 <TextAnnotator
                   id={id}
                   text={result}
